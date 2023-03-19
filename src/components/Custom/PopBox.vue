ba9bc69e301a4f3b47e692c49c54886ad6252ce9
Gabriel Guillou <pikatsuto@gmail.com>
2023-03-19 20:05:22 +0100

    feet: Add PopBox.vue

---
+++ src/components/Custom/PopBox.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="pop-box gradient-border flex">
+		<div class="pop-box-content favorites-folder content flex">
+
+			<div ref="popBoxMenu" class="flex width-100 hidden">
+				<slot></slot>
+			</div>
+
+			<PlusIcon class="plus-icon" @click="showPopBoxMenu" />
+		</div>
+	</div>
+</template>
+
+<script>
+import { PlusIcon } from "@heroicons/vue/20/solid";
+
+export default {
+	name: "PopBox",
+	components: {
+		PlusIcon
+	},
+	methods: {
+		showPopBoxMenu() {
+			this.$refs.popBoxMenu.classList.toggle("hidden");
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/style.scss";
+
+.pop-box {
+	position: absolute;
+	bottom: $light-len;
+	right: calc($light-len * 2);
+	z-index: 1;
+
+	max-width: calc(100% - $light-len * 4);
+
+	.pop-box-content {
+		align-items: end;
+	}
+
+	.plus-icon {
+		cursor: pointer;
+		padding: 0;
+	}
+}
+</style>
--- src/components/ModuleElement/ElementGroup.vue
+++ src/components/ModuleElement/ElementGroup.vue
@@ -1,8 +1,11 @@
 <template>
 	<main class="width-100">
 		<ul id="element-list" class="width-100 height-100 flex flex-warp list-none">
-			<li v-for="tab in tabList" :key="tab.id" :class="tab.active" class="auto-size show">
+			<li v-for="tab in tabList" :key="tab.id" :class="tab.active" class="auto-size show position-relative">
 				<component v-bind:is="tab.data.module" :data="tab.data"/>
+				<PopBox>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequuntur esse, excepturi natus nulla officia perspiciatis quod sint. Architecto aspernatur et illo iure minima praesentium quia reiciendis similique veritatis voluptates?</p>
+				</PopBox>
 			</li>
 		</ul>
 	</main>
@@ -11,9 +14,11 @@
 <script>
 import {computed} from "vue";
 import {useStore} from "vuex";
+import PopBox from "@/components/Custom/PopBox.vue";
 
 export default {
 	name: "ElementGroup",
+	components: {PopBox},
 	setup() {
 		const store = useStore();
 		const tabList = computed(() => store.state.tabList);
@@ -40,6 +45,10 @@ main {
 
 	#element-list li {
 		margin: calc($super-light-len / 2);
+	}
+
+	.position-relative{
+		position: relative;
 	}
 }
 </style>
